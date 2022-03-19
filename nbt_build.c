2d13567915bbbd03aab18dab79c90ee3205723c4
Azbantium <azbantium@firemail.cc>
2022-03-19 19:19:30 +0800

    merge functions in nbt_build to add_single

--- nbt_build.c
+++ nbt_build.c
@@ -6,10 +6,6 @@
 #include <string.h>
 #include <stdarg.h>
 
-
-
-
-
 static int nbt_add_raw_nbt(nbt_build* b, const int skip_size, const char* bytes, const int bytes_size, char* buf, const int buf_size)
 {
      if (skip_size > b->offset) return -1;
@@ -185,7 +181,7 @@ int nbt_start_list(nbt_build* b, char* buf, const int buf_len, char* name, const
     switch (b->top->state)
     {
     case S_OBJ_OR_CLOSE: {
-        total_len = 1 + 5 + 2 + name_len;
+        total_len = 1 + 2 + name_len + 5;
 
         char id = nbt_list;
         char len[2];
@@ -208,6 +204,7 @@ int nbt_start_list(nbt_build* b, char* buf, const int buf_len, char* name, const
         if (buf[b->top->payload] != (char)nbt_list) return -1;
 
         incre_list_meta(b, buf, buf_len);
+        if (nbt_allocate_raw_nbt(b, b->offset, 5, buf, buf_len)) return -1;
         break;
     }
 
@@ -241,304 +238,94 @@ int nbt_end_list(nbt_build* b, char* buf, const int buf_len)
     return 0;
 }
 
+int nbt_add_single(nbt_build* b, char* buf, const int buf_len, nbt_type_t type, char* name, const short name_len, char* nbt_payload, const int payload_len)
+{  
+    /* update state */
+    switch (b->top->state)
+    {
+    case S_OBJ_OR_CLOSE: {
+        int total_len = 1 + 2 + name_len + payload_len;
+
+        char _name_len[2];
+        swap_char_2((char*)&name_len, _name_len);
+
+        if (nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len)) return -1;
+        nbt_replace_raw_nbt(b, b->offset, (const char*)&type, 1, buf, buf_len);
+        nbt_replace_raw_nbt(b, b->offset, _name_len, 2, buf, buf_len);
+        nbt_replace_raw_nbt(b, b->offset, name, name_len, buf, buf_len);
+
+        break;
+    }
+
+    case S_LST_VAL_OR_CLOSE: {
+        nbt_replace_raw_nbt(b, b->top->payload, (const char*)&type, 1, buf, buf_len);
+
+        nbt_change_state(b, S_NXT_LST_VAL_OR_CLOSE);
+    } /* Fall through */
+    case S_NXT_LST_VAL_OR_CLOSE: {
+        if (buf[b->top->payload] != (char)type) return -1;
+
+        nbt_allocate_raw_nbt(b, b->offset, payload_len, buf, buf_len);
+
+        incre_list_meta(b, buf, buf_len);
+        break;
+    }
+
+    default:
+        return -1;
+        break;
+    }
+
+    nbt_replace_raw_nbt(b, b->offset, nbt_payload, payload_len, buf, buf_len);
+    return 0;
+}
+
 int nbt_add_char(nbt_build* b, char* buf, const int buf_len, char* name, const short name_len, char payload)
 {
     int total_len = 0;
     char nbt_payload = payload;
 
-    /* update state */
-    switch (b->top->state)
-    {
-    case S_OBJ_OR_CLOSE: {
-        total_len = 1 + 2 + name_len + 1;
-
-        char id = nbt_byte;
-
-        char _name_len[2];
-        swap_char_2((char*)&name_len, _name_len);
-
-        if (nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len)) return -1;
-        nbt_replace_raw_nbt(b, b->offset, &id, 1, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, _name_len, 2, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, name, name_len, buf, buf_len);
-
-        break;
-    }
-
-    case S_LST_VAL_OR_CLOSE: {
-        char id = nbt_byte;
-        nbt_replace_raw_nbt(b, b->top->payload, &id, 1, buf, buf_len);
-
-        nbt_change_state(b, S_NXT_LST_VAL_OR_CLOSE);
-    } /* Fall through */
-    case S_NXT_LST_VAL_OR_CLOSE: {
-        if (buf[b->top->payload] != (char)nbt_byte) return -1;
-
-        total_len = 1;
-        nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len);
-
-        incre_list_meta(b, buf, buf_len);
-        break;
-    }
-
-    default:
-        return -1;
-        break;
-    }
-
-    nbt_replace_raw_nbt(b, b->offset, &payload, 1, buf, buf_len);
-
-    return 0;
+    return nbt_add_single(b, buf, buf_len, nbt_byte, name, name_len, &nbt_payload, 1);
 }
 
 int nbt_add_short(nbt_build* b, char* buf, const int buf_len, char* name, const short name_len, short payload)
 {
-    int total_len = 0;
     char nbt_payload[4];
     swap_char_2((char*)&payload, nbt_payload);
-    
-    /* update state */
-    switch (b->top->state)
-    {
-    case S_OBJ_OR_CLOSE: {
-        total_len = 1 + 2 + name_len + 2;
 
-        char id = nbt_short;
-
-        char _name_len[2];
-        swap_char_2((char*)&name_len, _name_len);
-
-        if (nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len)) return -1;
-        nbt_replace_raw_nbt(b, b->offset, &id, 1, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, _name_len, 2, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, name, name_len, buf, buf_len);
-
-        break;
-    }
-
-    case S_LST_VAL_OR_CLOSE: {
-        char id = nbt_short;
-        nbt_replace_raw_nbt(b, b->top->payload, &id, 1, buf, buf_len);
-
-        nbt_change_state(b, S_NXT_LST_VAL_OR_CLOSE);
-    } /* Fall through */
-    case S_NXT_LST_VAL_OR_CLOSE: {
-        if (buf[b->top->payload] != (char)nbt_short) return -1;
-
-        total_len = 2;
-        nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len);
-
-        incre_list_meta(b, buf, buf_len);
-        break;
-    }
-
-    default:
-        return -1;
-        break;
-    }
-
-    nbt_replace_raw_nbt(b, b->offset, nbt_payload, 2, buf, buf_len);
-    return 0;
+    return nbt_add_single(b, buf, buf_len, nbt_short, name, name_len, nbt_payload, 2);
 }
 
 int nbt_add_integer(nbt_build* b, char* buf, const int buf_len, char* name, const short name_len, int payload)
 {
-    int total_len = 0;
     char nbt_payload[4];
     swap_char_4((char*)&payload, nbt_payload);
     
-    /* update state */
-    switch (b->top->state)
-    {
-    case S_OBJ_OR_CLOSE: {
-        total_len = 1 + 2 + name_len + 4;
-
-        char id = nbt_int;
-
-        char _name_len[2];
-        swap_char_2((char*)&name_len, _name_len);
-
-        if (nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len)) return -1;
-        nbt_replace_raw_nbt(b, b->offset, &id, 1, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, _name_len, 2, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, name, name_len, buf, buf_len);
-
-        break;
-    }
-
-    case S_LST_VAL_OR_CLOSE: {
-        char id = nbt_int;
-        nbt_replace_raw_nbt(b, b->top->payload, &id, 1, buf, buf_len);
-
-        nbt_change_state(b, S_NXT_LST_VAL_OR_CLOSE);
-    } /* Fall through */
-    case S_NXT_LST_VAL_OR_CLOSE: {
-        if (buf[b->top->payload] != (char)nbt_int) return -1;
-
-        total_len = 4;
-        nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len);
-
-        incre_list_meta(b, buf, buf_len);
-        break;
-    }
-
-    default:
-        return -1;
-        break;
-    }
-
-    nbt_replace_raw_nbt(b, b->offset, nbt_payload, 4, buf, buf_len);
-    return 0;
+    return nbt_add_single(b, buf, buf_len, nbt_int, name, name_len, nbt_payload, 4);
 }
 
 int nbt_add_long(nbt_build* b, char* buf, const int buf_len, char* name, const short name_len, long payload)
 {
-    int total_len = 0;
     char nbt_payload[8];
     swap_char_8((char*)&payload, nbt_payload);
     
-    /* update state */
-    switch (b->top->state)
-    {
-    case S_OBJ_OR_CLOSE: {
-        total_len = 1 + 2 + name_len + 8;
-
-        char id = nbt_long;
-
-        char _name_len[2];
-        swap_char_2((char*)&name_len, _name_len);
-
-        if (nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len)) return -1;
-        nbt_replace_raw_nbt(b, b->offset, &id, 1, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, _name_len, 2, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, name, name_len, buf, buf_len);
-
-        break;
-    }
-
-    case S_LST_VAL_OR_CLOSE: {
-        char id = nbt_long;
-        nbt_replace_raw_nbt(b, b->top->payload, &id, 1, buf, buf_len);
-
-        nbt_change_state(b, S_NXT_LST_VAL_OR_CLOSE);
-    } /* Fall through */
-    case S_NXT_LST_VAL_OR_CLOSE: {
-        if (buf[b->top->payload] != (char)nbt_long) return -1;
-
-        total_len = 8;
-        nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len);
-
-        incre_list_meta(b, buf, buf_len);
-        break;
-    }
-
-    default:
-        return -1;
-        break;
-    }
-
-    nbt_replace_raw_nbt(b, b->offset, nbt_payload, 8, buf, buf_len);
-    return 0;
+    return nbt_add_single(b, buf, buf_len, nbt_long, name, name_len, nbt_payload, 8);
 }
 
 int nbt_add_float(nbt_build* b, char* buf, const int buf_len, char* name, const short name_len, float payload)
 {
-    int total_len = 0;
     char nbt_payload[4];
     swap_char_4((char*)&payload, nbt_payload);
     
-    /* update state */
-    switch (b->top->state)
-    {
-    case S_OBJ_OR_CLOSE: {
-        total_len = 1 + 2 + name_len + 4;
-
-        char id = nbt_float;
-
-        char _name_len[2];
-        swap_char_2((char*)&name_len, _name_len);
-
-        if (nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len)) return -1;
-        nbt_replace_raw_nbt(b, b->offset, &id, 1, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, _name_len, 2, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, name, name_len, buf, buf_len);
-
-        break;
-    }
-
-    case S_LST_VAL_OR_CLOSE: {
-        char id = nbt_float;
-        nbt_replace_raw_nbt(b, b->top->payload, &id, 1, buf, buf_len);
-
-        nbt_change_state(b, S_NXT_LST_VAL_OR_CLOSE);
-    } /* Fall through */
-    case S_NXT_LST_VAL_OR_CLOSE: {
-        if (buf[b->top->payload] != (char)nbt_float) return -1;
-
-        total_len = 4;
-        nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len);
-
-        incre_list_meta(b, buf, buf_len);
-        break;
-    }
-
-    default:
-        return -1;
-        break;
-    }
-
-    nbt_replace_raw_nbt(b, b->offset, nbt_payload, 4, buf, buf_len);
-    return 0;
+    return nbt_add_single(b, buf, buf_len, nbt_float, name, name_len, nbt_payload, 4);
 }
 
 int nbt_add_double(nbt_build* b, char* buf, const int buf_len, char* name, const short name_len, double payload)
 {
-    int total_len = 0;
     char nbt_payload[8];
     swap_char_8((char*)&payload, nbt_payload);
     
-    /* update state */
-    switch (b->top->state)
-    {
-    case S_OBJ_OR_CLOSE: {
-        total_len = 1 + 2 + name_len + 8;
-
-        char id = nbt_double;
-
-        char _name_len[2];
-        swap_char_2((char*)&name_len, _name_len);
-
-        if (nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len)) return -1;
-        nbt_replace_raw_nbt(b, b->offset, &id, 1, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, _name_len, 2, buf, buf_len);
-        nbt_replace_raw_nbt(b, b->offset, name, name_len, buf, buf_len);
-
-        break;
-    }
-
-    case S_LST_VAL_OR_CLOSE: {
-        char id = nbt_double;
-        nbt_replace_raw_nbt(b, b->top->payload, &id, 1, buf, buf_len);
-
-        nbt_change_state(b, S_NXT_LST_VAL_OR_CLOSE);
-    } /* Fall through */
-    case S_NXT_LST_VAL_OR_CLOSE: {
-        if (buf[b->top->payload] != (char)nbt_double) return -1;
-
-        total_len = 8;
-        nbt_allocate_raw_nbt(b, b->offset, total_len, buf, buf_len);
-
-        incre_list_meta(b, buf, buf_len);
-        break;
-    }
-
-    default:
-        return -1;
-        break;
-    }
-
-    nbt_replace_raw_nbt(b, b->offset, nbt_payload, 8, buf, buf_len);
-    return 0;
+    return nbt_add_single(b, buf, buf_len, nbt_double, name, name_len, nbt_payload, 8);
 }
 
 int nbt_add_byte_array(nbt_build* b, char* buf, const int buf_len, char* name, const short name_len, char payload[], int payload_len)
